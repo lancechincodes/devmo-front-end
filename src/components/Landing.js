7b21d79135a62acbfb6a989da0923a898a04c7dc
lancechincodes <lanceechin@gmail.com>
2022-09-01 19:22:56 -0500

    Add initial landing page animations

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,65 @@
 import '../styles/Landing.css'
 import logo from '../assets/logo.svg'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { motion, AnimatePresence } from 'framer-motion'
+
+// animation for logo
+const logoVariants = {
+    hidden: {
+        opacity: 0
+    },
+    visible: {
+        opacity: [0, 1, 1, 1],
+        scale:  [1, 1, .4, 1],
+        rotate: [0, 0, 270, 0],
+        transition: {
+            duration: 2,
+            ease: "easeInOut",
+            delay: 1
+        },
+    },
+    leave: {
+        opacity: 0
+    }
+}
 
 function Landing() {
 
+    const [showLogo, setShowLogo] = useState(true)
+    const [showWelcome, setShowWelcome] = useState(false)
+
+
+    setTimeout(() => {
+        setShowLogo(false)
+    }, 4000)
+
+    setTimeout(() => {
+        setShowWelcome(true)
+    }, 5000)
+
+
     return (
-        <div>
-            Hello from the landing page
+        <div className="landing-page">
+            {/* Animate presence delays unmounting until delay animations are done */}
+            <AnimatePresence>
+                {showLogo && 
+                    <motion.img 
+                        key="logo"
+                        className="logo"
+                        variants={logoVariants}
+                        initial="hidden"
+                        animate="visible"
+                        exit="leave"
+                        src={logo} 
+                        alt="Devmo Logo"
+                />}
+            </AnimatePresence>
+
+            {showWelcome && 
+                <p>Welcome to Devmo</p>
+            }
+
         </div>
     );
 };
