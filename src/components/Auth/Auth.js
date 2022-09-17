ef72cb79bd6a8c574b9bf36df0a3ee09e834fbdd
lancechincodes <lanceechin@gmail.com>
2022-09-16 19:25:52 -0500

    Add MUI text field to auth page

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,9 +1,28 @@
 import './Auth.css'
+import TopNav from '../TopNav/TopNav'
+import { useReducer } from 'react'
+import TextField from '@mui/material/TextField';
+import { signUpFields } from './signUpFields';
 
 function Auth() {
     return (
-        <div>
-            Hello from the auth page
+        <div className="auth-page">
+            <TopNav/>
+            <form className="auth-form" type="submit">
+                <h1 className="auth-heading">Sign Up</h1>
+                <div className="text-fields">
+                    {signUpFields.map((textField, idx) => (
+                        <TextField
+                            key={idx} 
+                            className="outlined-basic" 
+                            label={textField.label}
+                            variant="outlined" 
+                            required={true}
+                            type={textField.type}
+                        />
+                    ))}
+                </div>
+            </form>
         </div>
     );
 };
---
+++ src/components/Auth/signUpFields.js
@@ -0,0 +1,22 @@
+export const signUpFields = [
+    {
+        label: "First name",
+        type: "text"
+    },
+    {
+        label: "Last name",
+        type: "text"
+    },
+    {
+        label: "Email",
+        type: "email"
+    },
+    {
+        label: "Password",
+        type: "password"
+    },
+    {
+        label: "Confirm password",
+        type: "password"
+    }
+]
