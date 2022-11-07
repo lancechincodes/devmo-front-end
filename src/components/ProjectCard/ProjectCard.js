7d93df9dd2a06c60a93ed796364c9d9635f87328
lancechincodes <lanceechin@gmail.com>
2022-11-06 23:32:24 -0600

    Complete first round of styling for project card

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,13 +1,11 @@
 import './ProjectCard.css'
 import axios from 'axios'
 import { useState, useEffect } from 'react';
-// import Heart from 'react-animated-heart'
 import TwitterLikeButton from 'twitter-like-button'
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function ProjectCard({project}) {
-    
 
     return (
         <div className="project-card">
@@ -15,28 +13,48 @@ function ProjectCard({project}) {
                 <img className="card-thumbnail" src={project.imageUrl} alt={`${project.name} Thumbnail`}/>
             </div>
             <div className="card-middle">
-                <h1 className="card-lg-text">
-                    {project.name.toUpperCase()}
-                </h1>
-                <p className="card-sm-text">
-                    {project.description.toUpperCase()}
-                </p>
-                <p className="card-sm-text">BUILT BY:</p>
-                <p className="card-md-text">
-                    {project.owner.firstName.toUpperCase() + ' ' + project.owner.lastName.toUpperCase()}
-                </p>
-                <p className="card-sm-text">BUILT WITH:</p>
-                {project.technologies.map((tech) => (
-                    <div key={tech} className="card-md-text">
-                        {tech.toUpperCase()}
+                <div className="card-top-section-container">
+                    <h1 className="card-lg-text">
+                        {project.name.toUpperCase()}
+                    </h1>
+                    <FontAwesomeIcon className="card-github-icon" icon={faGithub} />
+                </div>
+
+                <div className="card-section-container">
+                    <p className="card-sm-text">
+                        {project.description.toUpperCase()}
+                    </p>
+                </div>
+
+                <div className="card-section-container">
+                    <p className="card-sm-text">BUILT BY:</p>
+                    <p className="card-md-text">
+                        {project.owner.firstName.toUpperCase() + ' ' + project.owner.lastName.toUpperCase()}
+                    </p>
+                </div>
+
+                <div className="card-section-container">
+                    <p className="card-sm-text">BUILT WITH:</p>
+                    <div className="card-tech-chips">
+                        {project.technologies.map((tech, idx) => (
+                            <div key={idx} className="card-tech-chip">
+                                {idx !== project.technologies.length - 1 
+                                    ? <p className="card-md-text">{tech.toUpperCase()},&nbsp;&nbsp;</p> 
+                                    : <p className="card-md-text">{tech.toUpperCase()}</p> 
+                                }
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
             <hr/>
             <div className="card-bottom">
-                <h1 className="card-lg-text">#1</h1>
-                <div className="demo-btn">DEMO</div>
-                {/* <Heart/> */}
+                <h1 className="card-lg-text card-number">#1</h1>
+                <a href={project.projectUrl} target="_blank" rel="noopener noreferrer">
+                    <div className="demo-btn">
+                        <p className="card-sm-text">DEMO</p>
+                    </div>
+                </a>
                 <TwitterLikeButton/>
             </div>
         </div>
