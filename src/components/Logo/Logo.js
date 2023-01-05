93c0e82cac3c607801de7d84f9326147573ac485
lancechincodes <lanceechin@gmail.com>
2023-01-05 12:32:43 -0600

    Add on click listner to logo on top nav

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,6 +2,7 @@ import './Logo.css'
 import logo from '../../assets/logo.svg'
 
 function Logo({size}) {
+
     return (
         <div className="full-logo">
             <h1 className={`inline-logo-text-${size}`}>DEVM</h1>
